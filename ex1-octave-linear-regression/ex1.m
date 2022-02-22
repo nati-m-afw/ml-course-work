cc94edf6c7310312ce4e6dbf911ab5657dcb9a2d
nati-m <it.natnael.mekonnen@gmail.com>
2022-02-22 17:08:56 +0300

    uncomment plotting code

--- ex1-octave-linear-regression/ex1.m
+++ ex1-octave-linear-regression/ex1.m
@@ -44,7 +44,7 @@ m = length(y); % number of training examples
 
 % Plot Data
 % Note: You have to complete the code in plotData.m
-% plotData(X, y);
+plotData(X, y);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
@@ -84,8 +84,8 @@ fprintf(' -3.6303\n  1.1664\n\n');
 
 % Plot the linear fit
 hold on; % keep previous plot visible
-% plot(X(:,2), X*theta, '-')
-% legend('Training data', 'Linear regression')
+plot(X(:,2), X*theta, '-')
+legend('Training data', 'Linear regression')
 hold off % don't overlay any more plots on this figure
 
 % Predict values for population sizes of 35,000 and 70,000
@@ -122,14 +122,14 @@ end
 % transpose J_vals before calling surf, or else the axes will be flipped
 J_vals = J_vals';
 % Surface plot
-% figure;
-% surf(theta0_vals, theta1_vals, J_vals)
-% xlabel('\theta_0'); ylabel('\theta_1');
+figure;
+surf(theta0_vals, theta1_vals, J_vals)
+xlabel('\theta_0'); ylabel('\theta_1');
 
 % Contour plot
-% figure;
-% % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-% contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-% xlabel('\theta_0'); ylabel('\theta_1');
-% hold on;
-% plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+figure;
+% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+xlabel('\theta_0'); ylabel('\theta_1');
+hold on;
+plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
